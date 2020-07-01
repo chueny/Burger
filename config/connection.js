8b0a517a39a035a9a7bba66bcf8c020e06a044c4
Chue Yang <chueyangib@gmail.com>
2020-06-30 23:50:56 -0500

    update heroku

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,10 @@ var connection = mysql.createConnection({
   port: 3306,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
+  extra:{
+    ssl:true
+  }
 });
 
 // Make connection.
