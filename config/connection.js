3d9f7c33fc43d43ac6ff8604b5089d22ab02893d
Chue Yang <chueyangib@gmail.com>
2020-07-01 14:48:16 -0500

    mysql heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-// var connection = mysql.createConnection({
-//   host: process.env.HOST,
-//   port: process.env.PORT,
-//   user: c2hpo9z99mw8e3ix,
-//   password: c9qzlfej9088vie9,
-//   database: process.env.DATABASE
-// });
+var connection = mysql.createConnection({
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+});
 
 // Make connection.
 connection.connect(function(err) {
