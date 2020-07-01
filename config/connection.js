5acdd07369e535cec7356fdd4945b8abdfae4465
Chue Yang <chueyangib@gmail.com>
2020-06-30 23:44:13 -0500

    heroku link

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com,	
+  host: process.env.HOST,	
   port: 3306,
-  user: tso87gfnl5ba1rzi,
-  password: ngnxd3v3h2nelwv0,
-  database: dx24vnouh3fd5nn4
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
 
 // Make connection.
