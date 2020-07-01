038230605ee2bbed9722b2bca549e555dda2b816
Chue Yang <chueyangib@gmail.com>
2020-07-01 14:38:16 -0500

    Jaws connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-let connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+// var connection = mysql.createConnection({
+//   host: process.env.HOST,
+//   port: process.env.PORT,
+//   user: c2hpo9z99mw8e3ix,
+//   password: c9qzlfej9088vie9,
+//   database: process.env.DATABASE
+// });
 
 // Make connection.
 connection.connect(function(err) {
