c0a86344a98ab2396eec9e1572b74e8466a602e5
Chue Yang <chueyangib@gmail.com>
2020-07-01 19:52:27 -0500

    updated port

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-console.log("process", process.env.PORT);
-console.log("process", process.env.USERNAME);
-console.log("process", process.env.DATABASE);
-console.log("process", process.env.HOST);
-console.log("process", process.env.PASSWORD);
+console.log("port", process.env.DATABASE_PORT);
+console.log("username", process.env.USERNAME);
+console.log("database", process.env.DATABASE);
+console.log("host", process.env.HOST);
+console.log("pwd", process.env.PASSWORD);
 
 var connection = mysql.createConnection({
   host: process.env.HOST,
-  port: process.env.PORT,
+  port: process.env.DATABASE_PORT,
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE
